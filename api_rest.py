3f72f4c34fd21d4c1135bdb336538a72dd2e9c85
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-06 23:16:32 -0500

    Update api_rest.py

--- api_rest.py
+++ api_rest.py
@@ -2,3 +2,11 @@ from flask import Flask, request, jsonify
 import mysql.connector
 
 app = Flask(__name)
+# Configura la conexión a la base de datos MySQL
+
+db = mysql.connector.connect(
+    host="tu_host_mysql",
+    user="tu_usuario_mysql",
+    password="tu_contraseña_mysql",
+    database="tu_base_de_datos_mysql"
+)
